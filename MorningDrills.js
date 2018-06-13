901bd8608f59032e8b68f25e7747c3d3ca7b1157
Filipp Gorbunov <Filippgorb@gmail.com>
2018-06-13 09:24:57 -0700

    completed self-refrence

--- MorningDrills.js
+++ MorningDrills.js
@@ -28,3 +28,22 @@ function updateObject(obj){
 
 let myObject = {};
 console.log (updateObject(myObject));
+
+//Self-refrence
+
+function personMaker() {
+  let person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function() {
+      return console.log(`${this.firstName} and ${this.lastName}.`);
+    },
+  };
+  return person;
+}
+
+let ourPerson = personMaker(); 
+
+console.log(ourPerson.fullName());
