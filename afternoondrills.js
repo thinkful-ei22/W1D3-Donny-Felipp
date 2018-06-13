88a2362dbc81e02b842d81014e0044f834e8a482
Filipp Gorbunov <Filippgorb@gmail.com>
2018-06-13 11:45:45 -0700

    Completed 3 and 4 array problems

--- afternoondrills.js
+++ afternoondrills.js
@@ -5,7 +5,7 @@
 let loaf = {
   flour:300,
   water:210,
-  hydration:function(){return 100*(this.water/this.flour)}
+  hydration:function(){return 100*(this.water/this.flour);}
 };
 
 console.log(`loaf: ${loaf.flour} flour: ${loaf.water}`);
@@ -22,6 +22,38 @@ let myObject = {
 };
 
 for (let property in myObject){
-    console.log(`${property} : ${myObject[property]}`);
+  console.log(`${property} : ${myObject[property]}`);
+}
+
+// 3. Arrays in objects
+
+const food = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
 };
+
+console.log(food.meals[3]);
+
+// 4. Arrays of objects
+
+const personFilipp = {
+  name: 'Filipp',
+  jobTitle: 'web developer'
+};
+
+const personDonny = {
+  name: 'Donny',
+  jobTitle: 'Burger Flipper',
+};
+
+const personBob = {
+  name: 'Bob',
+  jobTitle: 'Builder',
+};
+
+const people = [personFilipp, personDonny, personBob];
+
+console.log(people.map(function(person) {
+  return `${person.name} is a ${person.jobTitle}.`;
+}));
+
 
